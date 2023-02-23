cf2a379e59df428d2807c2b4d8281287dd6d3026
Ederson Torresini <ederson.torresini@proton.me>
2023-02-23 12:34:49 +0000

    Melhorar formatação

--- cena.js
+++ cena.js
@@ -95,9 +95,21 @@ export default class CenaDeAbertura extends Phaser.Scene {
     this.physics.add.collider(this.bombs, this.platforms);
 
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    this.physics.add.overlap(
+      this.player,
+      this.stars,
+      this.collectStar,
+      null,
+      this
+    );
 
-    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+    this.physics.add.collider(
+      this.player,
+      this.bombs,
+      this.hitBomb,
+      null,
+      this
+    );
   }
 
   update() {
