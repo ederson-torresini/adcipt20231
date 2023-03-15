1fd952a396f7b081a6848c2e2192d8b644cea9f3
Ederson Torresini <ederson.torresini@proton.me>
2023-03-15 03:19:21 +0000

    Testar integração entre Socket.IO e Web APIs

--- cena.js
+++ cena.js
@@ -14,18 +14,20 @@ export default class CenaDeAbertura extends Phaser.Scene {
         this.txtCoords.setText(
           `Latitude: ${pos.coords.latitude}\nLongitude: ${pos.coords.longitude}`
         );
+        this.game.socket.emit("geolocation-api", pos.coords);
       },
       (err) => {
         console.error(`Erro (${err.code}): ${err.message}`);
       }
     );
+    //
     // Testar Vibration API
     this.btVibrationApi = this.add
       .text(50, 200, "Vibration API", { fill: "#FFFFFF" })
       .setInteractive()
       .on("pointerdown", () => {
         this.game.navigator.vibrate([1000]);
-        console.log("Vibration API");
+        this.game.socket.emit("vibration-api", "1000 ms");
       });
   }
 
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class Game extends Phaser.Game {
     this.socket.on("connect", () => {
       this.socket.emit("entrar-na-sala", this.socket.id);
     });
-
+    //
     this.navigator = window.navigator;
     this.scene.add("abertura", CenaDeAbertura);
     this.scene.start("abertura");
