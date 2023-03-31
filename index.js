2279e562b7254bd81acfa11ecf2f2d3429244583
Ederson Torresini <boidacarapreta@gmail.com>
2023-03-30 22:36:12 -0300

    Remover código de teste

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// Configuração do jogo
 import config from "./config.js";
-//
-// Importar código das cenas
+
 import abertura from "./abertura.js";
 import principal from "./principal.js";
 import fim_do_jogo from "./fim-do-jogo.js";
@@ -10,25 +8,12 @@ import final_feliz from "./final-feliz.js";
 class Game extends Phaser.Game {
   constructor() {
     super(config);
-    //
-    // Objetos globais
-    this.navigator = window.navigator;
-    try {
-      this.socket = io();
-      this.socket.on("connect", () => {
-        this.socket.emit("entrar-na-sala", this.socket.id);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-    //
-    // Carregar as cenas
+
     this.scene.add("abertura", abertura);
     this.scene.add("principal", principal);
     this.scene.add("fim-do-jogo", fim_do_jogo);
     this.scene.add("final-feliz", final_feliz);
-    //
-    // Iniciar pela cena de abertura
+
     this.scene.start("abertura");
   }
 }
