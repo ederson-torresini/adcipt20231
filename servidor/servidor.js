f839f576d263b4c03b1d8800691acb29d00f7c48
Ederson Torresini <etorresini@ifsc.edu.br>
2023-05-02 14:32:54 -0300

    Programar servidor com WebSocket Fixes #33

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3000;
 
+io.on("connection", (socket) => {
+  console.log("Cliente %s conectado ao servidor.", socket.id);
+
+  socket.on("disconnect", () => {});
+});
+
 app.use(express.static("../cliente/"));
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
+server.listen(PORT, () => console.log(`Servidor em execução na porta ${PORT}!`));
