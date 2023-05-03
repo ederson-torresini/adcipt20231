0c296d36e2367000f97f9a0068c48029a8c3cf66
Ederson Torresini <etorresini@ifsc.edu.br>
2023-05-03 14:10:17 -0300

    Programar salas de partida Fixes #34

--- cliente/js/cena-abertura.js
+++ cliente/js/cena-abertura.js
@@ -5,22 +5,107 @@ export default class abertura extends Phaser.Scene {
 
   preload() {
     this.load.image("ifsc-sj-2014", "./assets/ifsc-sj-2014.png");
+    this.load.image("grade", "./assets/grade.png");
   }
 
   create() {
-    /* Imagem de fundo */
-    this.imagem = this.add
-      .image(400, 225, "ifsc-sj-2014")
-      .setInteractive()
-      .on("pointerdown", () => {
-        this.imagem.destroy();
-        this.texto.destroy();
-        this.game.scene.start("principal");
-      });
+    this.imagem = this.add.image(400, 225, "ifsc-sj-2014");
+    this.grade = this.add.tileSprite(400, 225, 600, 300, "grade");
+    this.mensagem = this.add.text(100, 75, "Escolha uma sala para entrar:", {
+      fontFamily: "monospace",
+      font: "32px Courier",
+      fill: "#cccccc",
+    });
+    this.salas = [
+      {
+        numero: "0",
+        x: 150,
+        y: 125,
+        botao: undefined,
+      },
+      {
+        numero: "1",
+        x: 150,
+        y: 175,
+        botao: undefined,
+      },
+      {
+        numero: "2",
+        x: 150,
+        y: 225,
+        botao: undefined,
+      },
+      {
+        numero: "3",
+        x: 150,
+        y: 275,
+        botao: undefined,
+      },
+      {
+        numero: "4",
+        x: 150,
+        y: 325,
+        botao: undefined,
+      },
+      {
+        numero: "5",
+        x: 450,
+        y: 125,
+        botao: undefined,
+      },
+      {
+        numero: "6",
+        x: 450,
+        y: 175,
+        botao: undefined,
+      },
+      {
+        numero: "7",
+        x: 450,
+        y: 225,
+        botao: undefined,
+      },
+      {
+        numero: "8",
+        x: 450,
+        y: 275,
+        botao: undefined,
+      },
+      {
+        numero: "9",
+        x: 450,
+        y: 325,
+        botao: undefined,
+      },
+    ];
 
-    /* Texto em destaque */
-    this.texto = this.add.text(490, 50, "Clique no prédio para entrar...", {
-      fill: "#000000",
+    this.salas.forEach((item) => {
+      item.botao = this.add
+        .text(item.x, item.y, "[Sala " + item.numero + "]", {
+          fontFamily: "monospace",
+          font: "32px Courier",
+          fill: "#cccccc",
+        })
+        .setInteractive()
+        .on("pointerdown", () => {
+          this.salas.forEach((item) => {
+            item.botao.destroy();
+          });
+          this.game.sala = item.numero;
+          this.game.socket.emit("entrar-na-sala", this.game.sala);
+        });
+    });
+
+    this.game.socket.on("jogadores", (jogadores) => {
+      if (jogadores.segundo) {
+        this.mensagem.destroy();
+        this.game.jogadores = jogadores;
+        this.game.scene.start("principal");
+      } else if (jogadores.primeiro) {
+        this.grade.destroy();
+        this.imagem.destroy();
+        this.mensagem.setText("Aguardando segundo jogador...");
+      }
     });
   }
 
--- cliente/js/index.js
+++ cliente/js/index.js
@@ -6,22 +6,18 @@ import final_feliz from "./cena-final-feliz.js";
 
 class Game extends Phaser.Game {
   constructor() {
-    /* Configuração */
     super(config);
 
-    /* Estalecimento de canal bidirecional para troca de mensagens */
     this.socket = io();
     this.socket.on("connect", () => {
       console.log("Conectado ao servidor para troca de mensagens.")
     });
 
-    /* Cenas */
     this.scene.add("abertura", abertura);
     this.scene.add("principal", principal);
     this.scene.add("fim-do-jogo", fim_do_jogo);
     this.scene.add("final-feliz", final_feliz);
 
-    /* Primeira cena a carregar */
     this.scene.start("abertura");
   }
 }
